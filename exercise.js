f81fd8fd39819a55584175bed0dde479c160e3be
Tabarin01 <119614157+Tabarin01@users.noreply.github.com>
2023-08-03 15:47:17 +0200

    ex10 + comments ex1

--- exercise.js
+++ exercise.js
@@ -2,8 +2,8 @@
      Exercise 1 get the date and the time  *
 ********************************************/
 
-function getCurrentDay() {
-  const daysOfWeek = [
+function getCurrentDay() {   
+  const daysOfWeek = [  
     "Sunday",
     "Monday",
     "Tuesday",
@@ -11,19 +11,19 @@ function getCurrentDay() {
     "Thursday",
     "Friday",
     "Saturday",
-  ]; // array di stringhe
+  ]; // array di stringhe contenenti i giorni della settimana
   const date = new Date(); // oggetto Date di JS
-  const dayIndex = date.getDay();
-  return daysOfWeek[dayIndex];
+  const dayIndex = date.getDay(); 
+  return daysOfWeek[dayIndex];  // restituisce il giorno della settimana come stringa
 }
 
-function getCurrentTime() {
-  const date = new Date();
-  let hours = date.getHours();
-  const ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12;
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
+function getCurrentTime() { 
+  const date = new Date();  // istanza del tempo corrente
+  let hours = date.getHours(); // memorrizza in hours le ore correnti
+  const ampm = hours >= 12 ? "PM" : "AM";  //ternary operator le ore sono >= 12 ? se si "PM" se no "AM"
+  hours = hours % 12 || 12;  // Operzione che restituisce le ore nel formato a 12 e non 24
+  const minutes = String(date.getMinutes()).padStart(2, "0"); //ottiene i minuti e li converte in stringa assicurando sempre 2 cifre
+  const seconds = String(date.getSeconds()).padStart(2, "0"); //stessa cosa con i secondi
   return `${hours} ${ampm} : ${minutes} : ${seconds}`;
 }
 
@@ -144,27 +144,91 @@ let rotating = false;
 let originalString = "w3resource";
 
 function rotateString() {
-    const rotatedString = originalString.charAt(originalString.length - 1) + originalString.slice(0, -1);
-    document.getElementById("rotatedString").innerText = rotatedString;
-    originalString = rotatedString;
+  const rotatedString =
+    originalString.charAt(originalString.length - 1) +
+    originalString.slice(0, -1);
+  document.getElementById("rotatedString").innerText = rotatedString;
+  originalString = rotatedString;
 }
 
 function toggleRotation() {
-    if (!rotating) {
-        rotating = true;
-        intervalId = setInterval(rotateString, 200); // Adjust the interval (in milliseconds) for speed
-        document.getElementById("rotateButton").innerText = "Stop Rotation";
-    } else {
-        clearInterval(intervalId);
-        if (originalString === "w3resource") {
-            alert("NICE");
-        }
-        rotating = false;
-        document.getElementById("rotateButton").innerText = "Start Rotation";
+  if (!rotating) {
+    rotating = true;
+    intervalId = setInterval(rotateString, 200); // Adjust the interval (in milliseconds) for speed
+    document.getElementById("rotateButton").innerText = "Stop Rotation";
+  } else {
+    clearInterval(intervalId);
+    if (originalString === "w3resource") {
+      alert("NICE");
     }
+    rotating = false;
+    document.getElementById("rotateButton").innerText = "Start Rotation";
+  }
 }
 
+/******************************************* 
+Exercise 9  Xmas counter                   *
+********************************************/
+
+function daysLeftUntilChristmas() {
+  const today = new Date();
+  const currentYear = today.getFullYear();
+  const christmasDate = new Date(currentYear, 11, 25); // December is represented as 11 (0-based index)
+
+  // Check if Christmas has already passed this year, if yes, set the date for next year
+  if (today.getMonth() === 11 && today.getDate() > 25) {
+    christmasDate.setFullYear(currentYear + 1);
+  }
+
+  // Calculate the difference in time (in milliseconds) between today and Christmas
+  const timeDifference = christmasDate.getTime() - today.getTime();
+
+  // Convert the time difference to days
+  const daysLeft = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+
+  return daysLeft;
+}
+
+function updateDaysLeft() {
+  const daysLeft = daysLeftUntilChristmas();
+  const resultElement = document.getElementById("resultXmas");
+  resultElement.textContent = `There are ${daysLeft} days left until Christmas.`;
+}
+// Update days left on page load
+updateDaysLeft();
 
 /******************************************* 
-Exercise 6                  *
+Exercise 10  Multiply Divider              *
 ********************************************/
+
+function multiply() {
+  const number1 = parseFloat(document.getElementById("number1").value);
+  const number2 = parseFloat(document.getElementById("number2").value);
+  const resultElement = document.getElementById("resultCalc");
+
+  if (isNaN(number1) || isNaN(number2)) {
+    resultElement.textContent = "Please enter valid numbers.";
+    return;
+  }
+
+  const multiplicationResult = number1 * number2;
+  resultElement.textContent = `Result: ${multiplicationResult}`;
+}
+
+function divide() {
+  const number1 = parseFloat(document.getElementById("number1").value);
+  const number2 = parseFloat(document.getElementById("number2").value);
+  const resultElement = document.getElementById("resultCalc");
+
+  if (isNaN(number1) || isNaN(number2)) {
+    resultElement.textContent = "Please enter valid numbers.";
+    return;
+  }
+
+  if (number2 === 0) {
+    resultElement.textContent = "Error: Division by zero is not allowed.";
+  } else {
+    const divisionResult = number1 / number2;
+    resultElement.textContent = `Result: ${divisionResult}`;
+  }
+}
